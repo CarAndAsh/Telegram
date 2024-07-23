1969e5dfa50d8acb97c16589cf81f2c3cd316817
CarAndAsh <zeixes@gmail.com>
2024-07-23 17:24:33 +0300

    Example of book-bot

--- some_bot/handlers/user_handlers.py
+++ some_bot/handlers/user_handlers.py
@@ -1,10 +1,14 @@
+from copy import deepcopy
 from aiogram import Router, F
 from aiogram.filters import Command, CommandStart
-from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.types import Message, CallbackQuery
 
-from lexicon.lexicon_ru import LEXICON_RU, LEXICON_KB_RU
-from keyboards.keyboards import yn_keyboard, game_keyboard
-from services.services import get_bot_choice, get_winner
+from database.database import user_dict_template, users_db
+from filters.filters import IsDigitCallbackData, IsDelBookmarkCallbackData
+from keyboards.bookmarks_kb import build_bookmarks_kb, build_bookmarks_edit_kb
+from keyboards.pagination import build_pagination_kb
+from services.file_handling import book
+from lexicon.lexicon_ru import LEXICON_RU
 from log_config import logger
 
 user_router: Router = Router()
@@ -12,35 +16,117 @@ user_router: Router = Router()
 
 async def start_command(msg: Message):
     logger.debug('Player send start command')
-    await msg.answer(text=LEXICON_RU['/start'], reply_markup=yn_keyboard)
+    await msg.answer(text=LEXICON_RU['/start'])
+    if msg.from_user.id not in users_db:
+        users_db[msg.from_user.id] = deepcopy(user_dict_template)
 
 
 async def help_command(msg: Message):
     logger.debug('Player send help command')
-    await msg.answer(text=LEXICON_RU['/help'], reply_markup=yn_keyboard)
+    await msg.answer(text=LEXICON_RU['/help'])
 
 
-async def agree_btn(msg: Message):
-    logger.debug('Player push button for agree')
-    await msg.answer(text=LEXICON_KB_RU['yes'], reply_markup=game_keyboard)
+async def beginning_command(msg: Message):
+    users_db[msg.from_user.id]['page'] = 1
+    text = book[users_db[msg.from_user.id]['page']]
+    await msg.answer(
+        text=text,
+        reply_markup=build_pagination_kb(
+            'backward',
+            f'{users_db[msg.from_user.id]["page"]}/{len(book)}',
+            'forward'
+        )
+    )
 
 
-async def disagree_btn(msg: Message):
-    logger.debug('Player push button for disagree')
-    await msg.answer(text=LEXICON_KB_RU['disagree'], reply_markup=ReplyKeyboardRemove())
+async def continue_command(msg: Message):
+    text = book[users_db[msg.from_user.id]['page']]
+    await msg.answer(
+        text=text,
+        reply_markup=build_pagination_kb(
+            'backward',
+            f'{users_db[msg.from_user.id]["page"]}/{len(book)}',
+            'forward'
+        )
+    )
 
 
-async def game_btn(msg: Message):
-    bot_choice = get_bot_choice()
-    logger.debug('Player made his choise')
-    await msg.answer(text=f'{LEXICON_RU["bot_choice"]} {LEXICON_KB_RU[bot_choice]}')
-    winner = get_winner(msg.text, bot_choice)
-    await msg.answer(text=LEXICON_RU[winner], reply_markup=yn_keyboard)
+async def bookmarks_command(msg: Message):
+    page_num_set = users_db[msg.from_user.id]['bookmarks']
+    if page_num_set:
+        await msg.answer(text=LEXICON_RU[msg.text], reply_markup=build_bookmarks_kb(*page_num_set))
+    else:
+        await msg.answer(text=LEXICON_RU['no_bookmarks'])
 
+
+async def forward_press(callback: CallbackQuery):
+    if users_db[callback.from_user.id]['page'] < len(book):
+        users_db[callback.from_user.id]['page'] += 1
+        text = book[users_db[callback.from_user.id]['page']]
+        await callback.message.edit_text(text=text,
+                                         reply_markup=build_pagination_kb(
+                                             'backward',
+                                             f'{users_db[callback.from_user.id]["page"]}/{len(book)}',
+                                             'forward'))
+    else:
+        await callback.answer()
+
+
+async def backward_press(callback: CallbackQuery):
+    if users_db[callback.from_user.id]['page'] > 1:
+        users_db[callback.from_user.id]['page'] -= 1
+        text = book[users_db[callback.from_user.id]['page']]
+        await callback.message.edit_text(text=text,
+                                         reply_markup=build_pagination_kb(
+                                             'backward',
+                                             f'{users_db[callback.from_user.id]["page"]}/{len(book)}',
+                                             'forward'))
+    else:
+        await callback.answer()
+
+
+async def page_press(callback: CallbackQuery):
+    users_db[callback.from_user.id]['bookmarks'].add(users_db[callback.from_user.id]['page'])
+    await callback.answer('Страница добавлена в закладки')
+
+
+async def bookmark_press(callback: CallbackQuery):
+    text = book[int(callback.data)]
+    users_db[callback.from_user.id]['page'] = int(callback.data)
+    await callback.message.edit_text(text=text,
+                                     reply_markup=build_pagination_kb(
+                                         'backward',
+                                         f'{users_db[callback.from_user.id]["page"]}/{len(book)}',
+                                         'forward'))
+
+
+async def edit_press(callback: CallbackQuery):
+    await callback.message.edit_text(text=LEXICON_RU[callback.data],
+                                     reply_markup=build_bookmarks_edit_kb(
+                                         *users_db[callback.from_user.id]['bookmarks']))
+
+
+async def cancel_press(callback: CallbackQuery):
+    await callback.message.edit_text(text=LEXICON_RU['cancel_text'])
+
+
+async def del_press(callback: CallbackQuery):
+    users_db[callback.from_user.id]['bookmarks'].remove(int(callback.data[:-3]))
+    if users_db[callback.from_user.id]['bookmarks']:
+        await callback.message.edit_text(text=LEXICON_RU['/bookmarks'],
+                                         reply_markup=build_bookmarks_edit_kb(*users_db[callback.from_user.id]['bookmarks']))
+    else:
+        await callback.message.edit_text(text=LEXICON_RU['no_bookmarks'])
 
 user_router.message.register(start_command, CommandStart())
 user_router.message.register(help_command, Command(commands='help', prefix='/'))
-user_router.message.register(agree_btn, F.text == LEXICON_KB_RU['yes'])
-user_router.message.register(disagree_btn, F.text.in_((LEXICON_KB_RU['no'], LEXICON_KB_RU['remind'])))
-user_router.message.register(game_btn, F.text.in_(
-    (LEXICON_KB_RU.values())))
+user_router.message.register(beginning_command, Command(commands='beginning', prefix='/'))
+user_router.message.register(continue_command, Command(commands='continue', prefix='/'))
+user_router.message.register(bookmarks_command, Command(commands='bookmarks', prefix='/'))
+user_router.callback_query.register(forward_press, F.data == 'forward')
+user_router.callback_query.register(backward_press, F.data == 'backward')
+user_router.callback_query.register(page_press, lambda x: '/' in x.data and x.data.replace('/', '').isdigit())
+user_router.callback_query.register(bookmark_press, IsDigitCallbackData())
+user_router.callback_query.register(edit_press, F.data == 'edit_bookmarks')
+user_router.callback_query.register(cancel_press, F.data == 'cancel')
+user_router.callback_query.register(del_press, IsDelBookmarkCallbackData())
--- some_bot/main.py
+++ some_bot/main.py
@@ -1,34 +1,37 @@
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from asyncio import run
 
 from config_data.config import load_config, Config
 from handlers import user_handlers, other_handlers
-from keyboards.menu_comands import set_menu
+from keyboards.main_menu import set_menu
 
 from log_config import logger
 
 
-# TODO multiplayer between two and more players
 # TODO get name of users for logs
+# TODO adding books
+# TODO try int bookmark callback (because str to int and then int to str)
+# TODO correct work of _get_part_text func
 
 
 async def main_func():
     config: Config = load_config('./.env')
-    game_bot = Bot(config.tg_bot.token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    book_bot = Bot(config.tg_bot.token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher()
-    desc = 'Этот бот для игры в "Камень, ножницы, бумага, ящерица, Спок".'
-    name = 'Бот "Камень, ножницы, бумага, ящерица, Спок"'
+    desc = 'Этот бот для чтения книг...".'
+    name = 'Бот-читалка'
     dp.include_router(user_handlers.user_router)
     dp.include_router(other_handlers.other_router)
     logger.info('Start bot')
-    await game_bot.set_my_description(description=desc)
-    await game_bot.set_my_name(name=name)
-    await game_bot.delete_webhook(drop_pending_updates=True)
-    await set_menu(game_bot)
-    await dp.start_polling(game_bot)
+    if book_bot.get_my_description() != desc:
+        await book_bot.set_my_description(description=desc)
+    if book_bot.get_my_name() != name:
+        await book_bot.set_my_name(name=name)
+    await set_menu(book_bot)
+    await book_bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(book_bot)
 
 
 run(main_func())
