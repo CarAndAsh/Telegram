49d6e76de09ec61a40f0c74a5cd9df33ad2657ea
CarAndAsh <zeixes@gmail.com>
2025-12-18 20:42:25 +0300

    change msec format in logs

--- some_bot/bot_core/config.py
+++ some_bot/bot_core/config.py
@@ -7,12 +7,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 import logging
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-LOG_DEFAULT_FORMAT = "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
+LOG_DEFAULT_FORMAT = "[%(asctime)s] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
 
 
 class LogSettings(BaseModel):
     log_level: Literal['debug', 'info', 'warning', 'error', 'critical'] = 'info'
     log_format: str = LOG_DEFAULT_FORMAT
+    log_msec_format: str = '%s.%03d'
 
     @property
     def log_level_value(self) -> int:
--- some_bot/bot_core/log_cofig.py
+++ some_bot/bot_core/log_cofig.py
@@ -5,13 +5,14 @@ from sys import stdout
 from bot_core.config import settings, BASE_DIR
 
 formatter = Formatter(fmt=settings.log.log_format)
+formatter.default_msec_format = settings.log.log_msec_format
 
 stream = StreamHandler(stdout)
 stream.setFormatter(formatter)
 
 if 'logs' not in os.listdir(BASE_DIR):
     os.mkdir(BASE_DIR / 'logs')
-to_file = FileHandler(BASE_DIR / 'logs' / '.log.txt', 'w')
+to_file = FileHandler(BASE_DIR / 'logs' / '.log.txt', 'w', encoding='utf-8')
 to_file.setFormatter(formatter)
 
 logger = getLogger()
