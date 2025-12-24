2a8fb9ca07e0fc33a77d572e57bc44646255e831
CarAndAsh <zeixes@gmail.com>
2025-12-24 16:41:20 +0300

    rename main.py -> polling_run.py

---
+++ some_bot/bot_core/polling_run.py
@@ -0,0 +1,18 @@
+from asyncio import run
+
+from bot_core.bot import create_bot, create_dispatcher, name_and_desc_check_and_set
+from bot_core.log_cofig import logger
+
+logger.name = __file__
+
+async def main():
+    bot = create_bot()
+    await name_and_desc_check_and_set(bot)
+    logger.info('Конфигурация загружена')
+    dp = create_dispatcher()
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    logger.debug('Бот запущен')
+    run(main())
