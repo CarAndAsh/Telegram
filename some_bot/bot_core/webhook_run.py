7655e1f64c4e78fec319cbf19905dcb5530dbee5
CarAndAsh <zeixes@gmail.com>
2025-12-24 16:43:49 +0300

    prepare webhook run

---
+++ some_bot/bot_core/webhook_run.py
@@ -0,0 +1,50 @@
+from aiogram import Bot
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from aiohttp import web
+
+from bot_core.bot import create_bot, name_and_desc_check_and_set, create_dispatcher
+from bot_core.log_cofig import logger
+
+logger.name = __file__
+
+LOCAL_BOT_HOST = '0.0.0.0'
+LOCAL_BOT_PORT = 8080
+
+WEBHOOK_PATH = '/bots/webhook'
+WEBHOOK_SECRET = ''
+WEBHOOK_BASE_URL = 'https://aiogram.dev'
+
+
+async def on_startup(bot: Bot) -> None:
+    await name_and_desc_check_and_set(bot)
+    me = await bot.get_me()
+    logger.warning(f'Бот {me.username} запущен')
+    await bot.delete_webhook(drop_pending_updates=False)
+    await bot.set_webhook(
+        url=WEBHOOK_BASE_URL + WEBHOOK_PATH,
+        # secret_token=WEBHOOK_SECRET
+    )
+
+
+def create_prepared_web_app() -> web.Application:
+    bot = create_bot()
+    dp = create_dispatcher()
+    dp.startup.register(on_startup)
+
+    web_app = web.Application()
+    aiogram_webhook_req_handler = SimpleRequestHandler(
+        dispatcher=dp, bot=bot, handle_in_background=False,
+        # secret_token=WEBHOOK_SECRET
+    )
+    aiogram_webhook_req_handler.register(web_app, path=WEBHOOK_PATH)
+    setup_application(web_app, dp, bot=bot)
+    return web_app
+
+
+def main() -> None:
+    app = create_prepared_web_app()
+    web.run_app(app, host=LOCAL_BOT_HOST, port=LOCAL_BOT_PORT)
+
+
+if __name__ == '__main__':
+    main()
